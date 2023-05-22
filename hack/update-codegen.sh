35795ee64b5956e0f989ca0d875b629e983e4aaa
neaj <neaj@appscode.com>
2023-05-22 20:04:14 +0600

    update

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -9,3 +9,5 @@ neajmorshad.dev:v1beta1 \
 controller-gen rbac:roleName=controller-perms crd paths=github.com/Neaj-Morshad-101/crd-controller/pkg/apis/neajmorshad.dev/v1beta1 \
 crd:crdVersions=v1 output:crd:dir=/home/appscodepc/go/src/github.com/Neaj-Morshad-101/crd-controller/manifests output:stdout
 
+
+#controller-gen rbac:roleName=controller-perms crd paths=github.com/Neaj-Morshad-101/crd-controller/pkg/apis/neajmorshad.dev/v1beta1 crd:crdVersions=v1 output:crd:dir=$depelopmentDir/manifests output:stdout
